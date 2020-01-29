b2e50ce8a7e431d65de1325a5327556a46fc537b
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@P3B-1P4HGP2.dir.svc.accenture.com>
2020-01-29 05:25:27 -0600

    pom changed

--- src/test/java/casestudy/casestudy/RunnnerClass.java
+++ src/test/java/casestudy/casestudy/RunnnerClass.java
@@ -6,6 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Feature", glue = { "casestudy.casestudy" }, tags = "@login")
+@CucumberOptions(features = "Feature", glue = { "casestudy.casestudy" }, tags = "@login", plugin = {"pretty","json:target/cucumber.json"})
 public class RunnnerClass {
 }
